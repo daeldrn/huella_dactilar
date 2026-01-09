7a0465ecb7a3a1929505b1652ef59abc4b2cefe4
Dael David <daeldrn@gmail.com>
2026-01-09 14:39:57 -0500

    Update Javadoc files with new generation timestamp and change JSGD application title to "AllNovu Huella v2.5"
    - Updated all Javadoc files to reflect the new generation timestamp.
    - Changed the window title in JSGD.java to "AllNovu Huella v2.5".

--- netbeans_sample/src/SecuGen/FDxSDKPro/samples/JSGD.java
+++ netbeans_sample/src/SecuGen/FDxSDKPro/samples/JSGD.java
@@ -266,7 +266,7 @@ public class JSGD extends javax.swing.JFrame {
         jLabelSpacer1 = new javax.swing.JLabel();
         jLabelSpacer2 = new javax.swing.JLabel();
 
-        setTitle("AllNovu Huella v2.3");
+        setTitle("AllNovu Huella v2.5");
         addWindowListener(new java.awt.event.WindowAdapter() {
             public void windowClosing(java.awt.event.WindowEvent evt) {
                 exitForm(evt);
